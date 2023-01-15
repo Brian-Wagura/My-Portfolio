603c63209400572eb130c6d4b3a6ed49d6bfe4b2
wagura-droid <briankiraguri@gmail.com>
2023-01-15 21:50:41 +0300

    my portfolio first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/wagura_droid/" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a href="https://twitter.com/Wagura_Brian" className="home__social-icon" target="_blank">
+        <i class="uil uil-twitter"></i>
+      </a>
+
+      <a href="https://github.com/wagura-droid" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
